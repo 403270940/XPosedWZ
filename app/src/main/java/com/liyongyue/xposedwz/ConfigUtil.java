1a199d3d1a7aaf8386473efe430a5268a0b6953a
unknown <yli@SCS-YLI-PC.sychip.com.cn.local>
2015-07-06 14:17:31 +0800

    Initial commit

---
+++ app/src/main/java/com/liyongyue/xposedwz/ConfigUtil.java
@@ -0,0 +1,13 @@
+package com.liyongyue.xposedwz;
+
+import java.io.File;
+
+/**
+ * Created by yli on 2015/7/1.
+ */
+public class ConfigUtil {
+    private static String fileName = "";
+    static{
+        File configFile = new File(fileName);
+    }
+}
